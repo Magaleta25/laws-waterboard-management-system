2a2d70cd9e9de2e0dc8917ef7406a9c82a32addb
prescott25 <magaletaprescott25@gmail.com>
2024-11-06 18:40:26 +0200

    commit

--- index.php
+++ index.php
@@ -1,3 +1,64 @@
+<?php
+    require("connection.php");
+     //Check if email and password are not empty
+    if (isset($_POST['login'])) 
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Prepare and execute SQL query
+        $sql = "SELECT * FROM git WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Check if user exists
+        if ($result->num_rows > 0) 
+        {
+            $user = $result->fetch_assoc();
+             // Store user details in session
+            $_SESSION['email'] = $email;
+            $_SESSION['user_id'] = $user['ID'];
+            $_SESSION['user_role'] = $user['role'];
+
+            // Redirect based on user role
+            switch ($user['role']) 
+            {
+                case 'customer':
+                    header("Location: customer/homePage.php");
+                    break;
+                case 'admin':
+                    header("Location: admin/adminDashboard.php");
+                    break;
+                case 'technician':
+                    header("Location: technician/homePage.php");
+                    break;
+                case 'accountant':
+                    header("Location: accountant/homePage.php");
+                    break;
+                default:
+                    header("Location: index.php");
+            }
+                exit;
+        } 
+        else 
+        {
+            ?>
+           
+           <script>
+              alert("user does not exits");
+           </script>
+           <?php
+        }
+    }    
+?>
+
+
+
+
+
 <html>
 <head>
 <title></title>
@@ -10,7 +71,7 @@
         </div> 
     </header>
 <div>
-<form class="form" action="next.php" method="POST">
+<form class="form" action="index.php" method="POST">
         <div><label for="email">Email</label></div>
         <div>
         <input class="input"  type="email" placeholder="email" id="email" name="email">
